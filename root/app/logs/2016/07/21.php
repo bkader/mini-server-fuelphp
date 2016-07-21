a11f1e501d813bec9020ed620216b656942f182f
Kader Bouyakoub <Kader Bouyakoub>
2016-07-21 22:52:55 +0100

    First commit!

---
+++ root/app/logs/2016/07/21.php
@@ -0,0 +1,12 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2016-07-21 22:43:10 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:43:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+ERROR - 2016-07-21 22:43:44 --> Fatal Error - Class 'Confing' not found in D:\portable_php_apps\server_mini_fuelphp\root\app\classes\controller\welcome.php on line 32
+WARNING - 2016-07-21 22:43:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:43:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:44:08 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:44:22 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:44:44 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:45:56 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2016-07-21 22:46:01 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
